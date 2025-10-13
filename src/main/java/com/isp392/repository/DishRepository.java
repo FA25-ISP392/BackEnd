749d51891c55bd75f0c17866780cf3966315ae9b
Ngo Sy Bao Duy <bdbdh3208@gmail.com>
2025-10-13 15:06:03 +0700

    update xóa mềm

--- src/main/java/com/isp392/repository/DishRepository.java
+++ src/main/java/com/isp392/repository/DishRepository.java
@@ -4,10 +4,15 @@ import com.isp392.entity.Dish;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface DishRepository extends JpaRepository<Dish, Integer> {
     boolean existsByDishName(String dishName);
     Optional<Dish> findByDishName(String dishName);
+
+    // Thêm method để lấy món còn khả dụng
+    List<Dish> findAllByIsAvailableTrue();
+    Optional<Dish> findByDishIdAndIsAvailableTrue(int dishId);
 }
