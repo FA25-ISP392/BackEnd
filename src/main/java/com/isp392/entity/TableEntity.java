798be607087f90bdf5a45239ee29802b344f7f82
Nguyen Dang Ngoc Phong <ndnphong23@gmail.com>
2025-10-15 12:30:55 +0700

    tiny update tableentity + security

--- src/main/java/com/isp392/entity/TableEntity.java
+++ src/main/java/com/isp392/entity/TableEntity.java
@@ -19,7 +19,7 @@ public class TableEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     int tableId;
 
-    @Column(length = 255, nullable = false, unique = true)
+    @Column(length = 255, nullable = false)
     String tableName;
 
     @Column(nullable = false)
