fb69815d928918120a985f9faf4c831822d27281
Ngo Sy Bao Duy <bdbdh3208@gmail.com>
2025-10-29 11:14:38 +0700

    Update OpenApiConfig.java

--- src/main/java/com/isp392/configuration/OpenApiConfig.java
+++ src/main/java/com/isp392/configuration/OpenApiConfig.java
@@ -1,29 +1,29 @@
-//package com.isp392.configuration;
-//
-//import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-//import io.swagger.v3.oas.annotations.security.SecurityScheme;
-//import io.swagger.v3.oas.models.OpenAPI;
-//import io.swagger.v3.oas.models.servers.Server;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import java.util.List;
-//@SecurityScheme(
-//        name = "bearerAuth",
-//        type = SecuritySchemeType.HTTP,
-//        scheme = "bearer",
-//        bearerFormat = "JWT"
-//)
-//@Configuration
-//public class OpenApiConfig {
-//
-// @Bean
-// public OpenAPI customOpenAPI() {
-//     return new OpenAPI()
-//             .servers(List.of(
-////                       new Server().url("https://backend-production-0865.up.railway.app/isp392")
-//                     new Server().url("https://api-monngon88.purintech.id.vn/isp392")
-//             ));
-// }
-//}
-//
+package com.isp392.configuration;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+@Configuration
+public class OpenApiConfig {
+
+ @Bean
+ public OpenAPI customOpenAPI() {
+     return new OpenAPI()
+             .servers(List.of(
+//                       new Server().url("https://backend-production-0865.up.railway.app/isp392")
+                     new Server().url("https://api-monngon88.purintech.id.vn/isp392")
+             ));
+ }
+}
+
