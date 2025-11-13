6332aabb117ac14c65291c3bfb7e76434101b2d8
duyld_22 <ducduy.luong22@gmail.com>
2025-11-13 08:08:33 +0700

    Add orderId and tableId fields to OrderDetailResponse

--- src/main/java/com/isp392/dto/response/OrderDetailResponse.java
+++ src/main/java/com/isp392/dto/response/OrderDetailResponse.java
@@ -13,6 +13,8 @@ import java.util.List;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class OrderDetailResponse {
     Integer orderDetailId;
+    Integer orderId;
+    int tableId;
     Integer dishId;
     String dishName;
     Double totalPrice;
